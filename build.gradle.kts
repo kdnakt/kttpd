ec3ef047da81c2a217d93d28e5f357b2e5f761c9
kdnakt <a.kid.1985@gmail.com>
2020-12-03 02:59:53 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+  kotlin("multiplatform") version "1.4.20"
+}
+
+repositories {
+  mavenCentral()
+}
+
+kotlin {
+  macosX64("native") {
+    binaries {
+      executable()
+    }
+  }
+}
+
+tasks.withType<Wrapper> {
+  gradleVersion = "6.7.1"
+  distributionType = Wrapper.DistributionType.BIN
+}
