225ef228fb3f8863d749277c0724c556208a4d9f
kdnakt <a.kid.1985@gmail.com>
2020-12-05 07:51:22 +0900

    fix format

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,22 @@
 plugins {
-  kotlin("multiplatform") version "1.4.20"
+    kotlin("multiplatform") version "1.4.20"
 }
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 kotlin {
-  macosX64("native") {
-    binaries {
-      executable {
-        entryPoint = "com.kdnakt.kttpd.main"
-      }
+    macosX64("native") {
+        binaries {
+            executable {
+                entryPoint = "com.kdnakt.kttpd.main"
+            }
+        }
     }
-  }
 }
 
 tasks.withType<Wrapper> {
-  gradleVersion = "6.7.1"
-  distributionType = Wrapper.DistributionType.BIN
+    gradleVersion = "6.7.1"
+    distributionType = Wrapper.DistributionType.BIN
 }
