aa90583e9fd0c4edf454a4fde9c2dd18e8e0cb71
kdnakt <a.kid.1985@gmail.com>
2020-12-19 17:05:23 +0900

    use toInt

--- src/nativeMain/kotlin/com/kdnakt/kttpd/URLDecoder.kt
+++ src/nativeMain/kotlin/com/kdnakt/kttpd/URLDecoder.kt
@@ -2,23 +2,6 @@ package com.kdnakt.kttpd
 
 import kotlinx.cinterop.toKString
 
-private const val CHAR_COLON = 58
-private const val INDEX_ZERO = 48
-private const val INDEX_A    = 55
-
-fun String.toByteObj(): Byte {
-    val cArr = toUpperCase().toCharArray()
-    var res = 0
-    var p = 1
-    for (i in cArr.size - 1 downTo 0) {
-        val c = cArr[i].toInt()
-        val sub = if (c <= CHAR_COLON) INDEX_ZERO else INDEX_A
-        res += (c - sub) * p
-        p *= 16
-    }
-    return res.toByte()
-}
-
 fun decodeURL(url: String): String {
     val res = StringBuilder()
     var i = 0
@@ -33,7 +16,7 @@ fun decodeURL(url: String): String {
                 var curr = c
                 while (i + 2 < url.length && '%' == curr) {
                     val hex = url.substring(i + 1, i + 3)
-                    bArr += hex.toByteObj()
+                    bArr += hex.toInt(16).toByte()
                     i += 3
                     if (i < url.length) {
                         curr = url[i]
